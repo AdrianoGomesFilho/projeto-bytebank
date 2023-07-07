31288f666d63c2c4c21e529a3b30c003eff35df1
Adriano G <adrianogomesfilho98@gmail.com>
2023-07-06 21:01:14 -0300

    .

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import { ContaCorrente } from "./ContaCorrente.js"
 import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Marcela", 12122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
+const contaCorrenteMarcela = new ContaCorrente(cliente2, 1002);
+console.log(contaCorrenteMarcela.saldo);
+/* contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new Conta(50, cliente1, 1001);
@@ -13,3 +16,4 @@ const contaPoupanca = new Conta(50, cliente1, 1001);
 contaCorrenteRicardo.teste();
 
 console.log(contaCorrenteRicardo);
+console.log(ContaCorrente.numeroDeContas) */
--- teste.js
+++ teste.js
@@ -1,17 +1,20 @@
 class Pessoa {
-  constructor(nome, idade, sexo) {
-     this.nome = nome;
-     this.idade = idade;
-     this.sexo = sexo;
-  }
+   constructor(nome, idade, sexo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.sexo = sexo;
+   }
 }
 
 class Matheus extends Pessoa {
-  constructor(nomeDiferente, algumaIdade, meuSobrenome) {
-     super(nomeDiferente, algumaIdade);  //As variáveis têm nomes diferentes da classe pai, mas funcionam como uma função normalmente.
-     // Cuidado: como não foi passado um terceiro parâmetro, o atributo 'sexo' dos objetos instanciados terão seu conteúdo como "undefined"
-     this.sobrenome = meuSobrenome;
-  }
+   constructor(nomeDiferente, algumaIdade, meuSobrenome) {
+      super(nomeDiferente, algumaIdade);  
+//As variáveis têm nomes diferentes da classe pai, mas funcionam como uma função normalmente.
+// Cuidado: como não foi passado um terceiro parâmetro, o atributo 'sexo' dos objetos instanciados terão seu conteúdo como "undefined"
+      this.sobrenome = meuSobrenome;
+   }
 }
 
-console.log(Matheus.meuSobrenome)
+const pessoa1 = new Pessoa('fulaninho')
+
+console.log(pessoa1)
