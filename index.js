f92402ad3cb9c7ddbd3369b00e068388cfee7818
Adriano G <adrianogomesfilho98@gmail.com>
2023-07-25 20:24:47 -0300

    .

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ diretor.cadastrarSenha("123456")
 const gerente = new Gerente("Ricardo", 5000, 1237896013);
 gerente.cadastrarSenha("123");
 
-const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const cliente = new Cliente("Lais", 78989799889, "456");
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
 
 
-console.log(gerenteEstaLogado, diretorEstaLogado);
 
